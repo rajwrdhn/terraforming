ea582a6f18457eeb49a38be035807fb2e8d92baa
rajwrdhn <rajwrdhn@gmail.com>
2023-06-03 06:11:20 +0200

    var vpc

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -13,4 +13,15 @@ provider "aws" {
 
 resource "aws_vpc" "vpcname" {
     cidr_block = "10.0.0.0/18"
+    tags = {
+      Name = var.vpcname
+    }
 }
+
+variable "vpcname" {
+  type = string
+  default = "myvpc"  
+}
+
+#export AWS_ACCESS_KEY_ID = 
+#export AWS_SECRET_KEY_ID = 
